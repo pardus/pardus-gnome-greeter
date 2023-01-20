164907bf1b07de8afad381999971f34dfd11b2da
osman <osman.coskun@pardus.org.tr>
2023-01-20 13:08:21 +0300

    setup files are added

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,35 +1,42 @@
 import gi
 import os
 import subprocess
+
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+gi.require_version("Gdk", "3.0")
+from gi.repository import Gtk,Gdk
+
 from LayoutChanger import LayoutChanger
+
 layoutChanger = LayoutChanger()
 
 class MainWindow():
     def __init__(self,application):
 
+        screen = Gdk.Screen.get_default()
+        provider = Gtk.CssProvider()
+        provider.load_from_path(os.path.dirname(os.path.abspath(__file__))+"/../css/style.css")
+        Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
         # GLADE
         self.builder = Gtk.Builder()
-        self.builder.add_from_file(os.path.dirname(os.path.abspath(__file__)) + "/../ui/ui.glade")
+        self.builder.add_from_file(os.path.dirname(os.path.abspath(__file__)) + "/../ui/ui2.glade")
         self.builder.connect_signals(self)
 
         layout_name = self.get_layout_name().split("'")[1]
 
-        # RADIO BUTTONS
-        radio_buttons = {
+        # BUTTONS
+        buttons = {
             "set_1":self.builder.get_object("set_1"),
             "set_2":self.builder.get_object("set_2"),
             "set_3":self.builder.get_object("set_3"),
             "set_4":self.builder.get_object("set_4"),
         }
 
-        for radio in radio_buttons:
-            radio_buttons[radio].connect("toggled",self.apply_layout,radio)
-            print(radio)
-            if layout_name == radio:
-                print("radio : %s"%radio)
-                radio_buttons[radio].set_active(True)
+        for button in buttons:
+            buttons[button].connect("clicked",self.apply_layout,buttons,button)
+            if button == layout_name:
+                buttons[button].get_style_context().add_class("btn-active")
 
 
         # WINDOW
@@ -38,13 +45,17 @@ class MainWindow():
         self.window.connect("destroy",self.quit_window)
         self.window.show_all()
 
-    def apply_layout(self,action,name):
-        layoutChanger.set_layout(name)
-
+    def apply_layout(self,action,button_group,button):
+        layoutChanger.set_layout(button)
+        for btn in button_group:
+            if btn==button:
+                button_group[btn].get_style_context().add_class("btn-active")
+            else:
+                button_group[btn].get_style_context().remove_class("btn-active")   
+        
     def get_layout_name(self):
         cmd = "gsettings get org.pardus.pardus-gnome-greeter layout-name"
         return subprocess.getoutput(cmd)
     
     def quit_window(self,action):
         self.window.get_application().quit()
-    
