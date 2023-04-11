b2954d60b32a47b8d2ec18f619c14d4389a8aa6b
Osman Coskun <osman.coskun@pardus.org.tr>
2023-04-11 11:54:31 +0300

    build test

--- src/Main.py
+++ src/Main.py
@@ -2,15 +2,34 @@
 
 import gi,sys
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+from gi.repository import Gtk,Gio,GLib
 
 class Application(Gtk.Application):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, application_id="tr.org.pardus.pardus-gnome-greeter")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,  application_id="tr.org.pardus.pardus-gnome-greeter",
+            flags=Gio.ApplicationFlags(8), **kwargs)
         self.connect('activate', self.on_activate)
         self.main_window = None
 
+
+        self.add_main_option(
+            "details",
+            ord("d"),
+            GLib.OptionFlags(0),
+            GLib.OptionArg(1),
+            "Details page of application",
+            None,
+        )
+
+        self.add_main_option(
+            "remove",
+            ord("r"),
+            GLib.OptionFlags(0),
+            GLib.OptionArg(1),
+            "Remove page of application",
+            None,
+        )
     def on_activate(self, app):
         if not self.main_window:
             from MainWindow import MainWindow
@@ -18,6 +37,11 @@ class Application(Gtk.Application):
             self.main_window.set_application(self)
             self.main_window.present()
 
-
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        options = options.end().unpack()
+        self.args = options
+        self.activate()
+        return 0
 app = Application()
 app.run(sys.argv)
