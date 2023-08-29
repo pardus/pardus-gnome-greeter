12b26a743f9184bc9c00144c1d6a08bdf9fbb88f
qwe <osman.coskun@pardus.org.tr>
2023-08-29 18:53:36 +0300

    feat: nautilus and desktop icon scaling added

--- src/Pages/Display.py
+++ src/Pages/Display.py
@@ -14,38 +14,80 @@ TRANSLATIONS_PATH = "/usr/share/locale"
 
 locale.bindtextdomain(APPNAME_CODE, TRANSLATIONS_PATH)
 locale.textdomain(APPNAME_CODE)
+di_options = {
+    0.0: "tiny",
+    1.0: "small",
+    2.0: "standard",
+    3.0: "large",
+    "'tiny'": 0.0,
+    "'small'": 1.0,
+    "'standard'": 2.0,
+    "'large'": 3.0,
+}
+ni_options = {
+    0.0: "small",
+    1.0: "small-plus",
+    2.0: "medium",
+    3.0: "large",
+    4.0: "extra-large",
+    "'small'": 0.0,
+    "'small-plus'": 1.0,
+    "'medium'": 2.0,
+    "'large'": 3.0,
+    "'extra-large'": 4.0,
+}
+
+
+desktop_schema = "org.gnome.desktop.interface"
+desktop_key = "text-scaling-factor"
+
+desktop_icons_schema = "org.gnome.shell.extensions.ding"
+desktop_icons_key = "icon-size"
+
+nautilus_schema = "org.gnome.nautilus.icon-view"
+nautilus_key = "default-zoom-level"
 
 
 def fun_change_display_scale(widget):
     value = widget.get_value()
     scale = float(1 + (0.25 * value))
-
-    schema = "org.gnome.desktop.interface"
     key = "text-scaling-factor"
-    Ptk.utils.gsettings_set(schema, key, scale)
+    Ptk.utils.gsettings_set(desktop_schema, key, scale)
 
 
 def fun_change_cursor_scale(widget):
-    schema = "org.gnome.desktop.interface"
     key = "cursor-size"
     value = int(widget.get_name())
     if widget.get_active():
-        Ptk.utils.gsettings_set(schema, key, value)
+        Ptk.utils.gsettings_set(desktop_schema, key, value)
+
 
 def fun_change_desktop_icons_scale(widget):
-    di_options = {
-        0.0:'tiny',
-        1.0:'small',
-        2.0:'standard',
-        3.0:'large'
-    }
-    schema = "org.gnome.shell.extensions.ding"
-    key = "icon-size"
     widget_value = widget.get_value()
     value = di_options[widget_value]
-    Ptk.utils.gsettings_set(schema,key,value)
+    Ptk.utils.gsettings_set(desktop_icons_schema, desktop_icons_key, value)
 
-    
+
+def fun_change_nautilus_icons_scale(widget):
+    widget_value = widget.get_value()
+    value = ni_options[widget_value]
+    Ptk.utils.gsettings_set(nautilus_schema, nautilus_key, value)
+
+
+def fun_set_values_to_scale(desktop, nautilus):
+    desktop_icon_size = str(
+        Ptk.utils.gsettings_get(desktop_icons_schema, desktop_icons_key)
+    )
+    nautilus_icon_size = str(Ptk.utils.gsettings_get(nautilus_schema, nautilus_key))
+    print(desktop_icon_size, nautilus_icon_size)
+    print(type(desktop_icon_size), type(nautilus_icon_size))
+    di_value = di_options[desktop_icon_size]
+    print(di_value)
+    ni_value = ni_options[nautilus_icon_size]
+    print(ni_value)
+    desktop.set_value(di_value)
+    nautilus.set_value(ni_value)
+
 
 def fun_create():
     # RETURNING DISPLAY BOX
@@ -98,7 +140,6 @@ def fun_create():
     markup = f"<b>{_('Recommended scale option for main display is')} {get_recommended_scale()}%</b>"
     ui_recommended_scale_label = Ptk.Label(markup=markup)
 
-
     # masaustu simge boyutu
     ui_desktop_icons_scale = Ptk.Scale(
         value=0.0,
@@ -108,20 +149,17 @@ def fun_create():
         page_increment=1.0,
         page_size=0.0,
         restrict_to_fill_level=True,
-        round_digits=0.0
+        round_digits=0.0,
     )
     ui_desktop_icons_scale.add(0, "TOP", _("Tiny"))
     ui_desktop_icons_scale.add(1, "TOP", _("Small"))
     ui_desktop_icons_scale.add(2, "TOP", _("Standart"))
     ui_desktop_icons_scale.add(3, "TOP", _("Large"))
 
-    ui_desktop_icons_scale.connect("value-changed",fun_change_desktop_icons_scale)
-
+    ui_desktop_icons_scale.connect("value-changed", fun_change_desktop_icons_scale)
 
     ui_desktop_icons_label_markup = f"<b>{_('Desktop icon size')}</b>"
     ui_desktop_icons_label = Ptk.Label(markup=ui_desktop_icons_label_markup)
-
-
 
     # dosya yoneticisi simge boyutu
     ui_nautilus_icons_scale = Ptk.Scale(
@@ -132,7 +170,7 @@ def fun_create():
         page_increment=1.0,
         page_size=0.0,
         restrict_to_fill_level=True,
-        round_digits=0.0
+        round_digits=0.0,
     )
     ui_nautilus_icons_scale.add(0, "TOP", _("Tiny"))
     ui_nautilus_icons_scale.add(1, "TOP", _("Small"))
@@ -140,8 +178,12 @@ def fun_create():
     ui_nautilus_icons_scale.add(3, "TOP", _("Large"))
     ui_nautilus_icons_scale.add(4, "TOP", _("Extra Large"))
 
+    ui_nautilus_icons_scale.connect("value-changed", fun_change_nautilus_icons_scale)
+
     ui_nautilus_icons_label_markup = f"<b>{_('File manager icon size')}</b>"
     ui_nautilus_icons_label = Ptk.Label(markup=ui_nautilus_icons_label_markup)
+
+    fun_set_values_to_scale(ui_desktop_icons_scale, ui_nautilus_icons_scale)
 
     temporary_icon = Ptk.Image(
         file=cur_dir + "/../../data/assets/cursor.svg", pixel_size=12
@@ -197,15 +239,11 @@ def fun_create():
             # scale
             ui_recommended_scale_label,
             ui_display_scale,
-
             ui_desktop_icons_label,
             ui_desktop_icons_scale,
-
-
             # file man scale
             ui_nautilus_icons_label,
             ui_nautilus_icons_scale,
-
             ui_font_scale_label,
             ui_cursor_buttons_box,
         ],
