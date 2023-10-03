303a0e9f2ddf2da22cd9c7780e4e965359991bf4
qwe <osman.coskun@pardus.org.tr>
2023-10-03 14:54:03 +0300

    fix: wallpaper setting on both dark/light modes

--- src/WallpaperManager.py
+++ src/WallpaperManager.py
@@ -1,29 +1,17 @@
-import subprocess
-import gi
 import os
 import sys
-import time
-import glob
 
 sys.path.append("../")
-gi.require_version("Gtk", "4.0")
-gi.require_version("Gdk", "4.0")
-from gi.repository import Gtk, Gdk, Gio, GObject, GLib
-from utils import get_current_theme
 from libpardus import Ptk
 
 
 class WallpaperManager:
     def change_wallpaper(self, picture_uri):
-        theme = str(get_current_theme())
         theme_schema = "org.gnome.desktop.background"
-        theme_keys = {
-            "'prefer-dark'": "picture-uri-dark",
-            "'default'": "picture-uri",
-        }
-        return Ptk.utils.gsettings_set(
-            theme_schema, theme_keys[theme], "file://" + picture_uri
-        )
+        theme_keys = ["picture-uri-dark", "picture-uri"]
+        for theme in theme_keys:
+            Ptk.utils.gsettings_set(theme_schema, theme, "file://" + picture_uri)
+        return True
 
     def get_wallpapers(self):
         wallpaper_dir = "/usr/share/backgrounds"
