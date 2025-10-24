9352fbb24ab121a31c2263d918f1d8f147b6a73f
Osman Coskun <osman.coskun@pardus.org.tr>
2025-10-24 14:20:29 +0300

    refactor(build): Clean up build script and fix desktop file

--- update-translations.sh
+++ update-translations.sh
@@ -9,15 +9,30 @@ then
 	exit
 fi
 
-echo "updating pot file"
-xgettext -o data/po/pardus-gnome-greeter.pot --files-from=data/po/POTFILES
+echo "Updating pot file..."
+
+# Define source files for different parsers
+PY_FILES=$(grep '.py$' data/po/POTFILES | tr '\n' ' ')
+UI_FILES=$(grep -E '.ui$|.desktop.in$' data/po/POTFILES | tr '\n' ' ')
+
+# Extract from Python files
+xgettext -o data/po/py.pot -L Python $PY_FILES
+
+# Extract from UI and .desktop files
+xgettext -o data/po/ui.pot -L Glade --keyword=_ --keyword=Name --keyword=Comment --keyword=Keywords $UI_FILES
+
+# Merge pot files
+msgcat --sort-output data/po/py.pot data/po/ui.pot > data/po/pardus-gnome-greeter.pot
+
+# Clean up temporary files
+rm data/po/py.pot data/po/ui.pot
 
 for lang in ${langs[@]}; do
 	if [[ -f data/po/$lang.po ]]; then
-		echo "updating $lang.po"
-		msgmerge -o data/po/$lang.po data/po/$lang.po data/po/pardus-gnome-greeter.pot
+		echo "Updating $lang.po"
+		msgmerge -U data/po/$lang.po data/po/pardus-gnome-greeter.pot
 	else
-		echo "creating $lang.po"
+		echo "Creating $lang.po"
 		cp data/po/pardus-gnome-greeter.pot data/po/$lang.po
 	fi
 done 
