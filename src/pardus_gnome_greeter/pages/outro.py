d31c17a8be497d51cb84a3c8d408cc219e0ab4b5
Osman Coskun <osman.coskun@pardus.org.tr>
2025-11-22 02:05:32 +0300

    feat(outro): redesign outro page

--- src/pardus_gnome_greeter/pages/outro.py
+++ src/pardus_gnome_greeter/pages/outro.py
@@ -8,7 +8,31 @@ from gi.repository import Gtk, Adw
 @Gtk.Template(resource_path='/tr/org/pardus/pardus-gnome-greeter/ui/OutroPage.ui')
 class OutroPage(Adw.PreferencesPage):
     __gtype_name__ = 'OutroPage'
+    
+    explore_button = Gtk.Template.Child("explore_button")
+    
+    # Link buttons
+    btn_website = Gtk.Template.Child("btn_website")
+    btn_docs = Gtk.Template.Child("btn_docs")
+    btn_community = Gtk.Template.Child("btn_community")
+    btn_forum = Gtk.Template.Child("btn_forum")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        print("OutroPage created.") 
+        
+        self.explore_button.connect("clicked", self.on_explore_clicked)
+        
+        # Connect link buttons
+        self.btn_website.connect("clicked", self.on_link_clicked, "https://www.pardus.org.tr/")
+        self.btn_docs.connect("clicked", self.on_link_clicked, "https://belge.pardus.org.tr/")
+        self.btn_community.connect("clicked", self.on_link_clicked, "https://gonullu.pardus.org.tr/")
+        self.btn_forum.connect("clicked", self.on_link_clicked, "https://forum.pardus.org.tr/")
+
+    def on_explore_clicked(self, button):
+        root = self.get_root()
+        if root:
+            root.close()
+            
+    def on_link_clicked(self, button, uri):
+        """Open the given URI in default browser"""
+        Gtk.show_uri(self.get_native(), uri, 0)
